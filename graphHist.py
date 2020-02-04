531f2e2100aa1dee215eaaa332bb94c2d2335392
thomas-osgood <tosgood1@gmail.com>
2020-02-03 20:33:27 -0500

    Update
    Figure now auto-saved to picture file with date and time stamp in file name.

--- graphHist.py
+++ graphHist.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from datetime import datetime
 import matplotlib.pyplot as plt
 
 letterA = []
@@ -12,8 +13,15 @@ with open(filename,'r') as f:
         letterA.append(a[0])
         letterC.append(int(a[1]))
 
+figname = "saved_figs/cryptograph_{0}.jpg".format(datetime.now().strftime("%Y%m%d_%H%M%S"))
 fig = plt.figure()
 plt.bar(letterA,letterC)
 plt.xlabel("Letters")
 plt.ylabel("Count")
+try:
+    plt.savefig(figname)
+    print("[+] Histogram Saved To {0}".format(figname))
+except:
+    print("[!] Unable To Save Figure")
 plt.show()
+
