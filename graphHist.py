66e4d92cab3dfd06bedea8c975d03d8cb1755550
thomas-osgood <tosgood1@gmail.com>
2020-02-27 17:50:21 -0500

    Commented
    Added comments to the code.

--- graphHist.py
+++ graphHist.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
 
+"""
+Import Required Libraries
+"""
 from datetime import datetime
 import matplotlib.pyplot as plt
 
+"""
+Initialize Variables.
+"""
 letterA = []
 letterC = []
 filename = "histogram.txt"
 
+"""
+Open Histogram File And Build
+Lists Used To Graph.
+"""
 with open(filename,'r') as f:
     for line in f:
         a = line.split(':')
         letterA.append(a[0])
         letterC.append(int(a[1]))
 
+"""
+Plot Histogram & Save Plot To File.
+File Is Date And Time Stamped.
+"""
 figname = "saved_figs/cryptograph_{0}.jpg".format(datetime.now().strftime("%Y%m%d_%H%M%S"))
 fig = plt.figure()
 plt.bar(letterA,letterC)
